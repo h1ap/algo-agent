77f94eaae7f4964e39ba4da767f54c76026b8527
heap <heap.aazz@gmail.com>
2025-04-28 18:03:35 +0800

    refactor(docker): 更新配置文件扩展名
    - 将配置文件扩展名从 .yml 改为 .yaml
    - 保持与其他配置文件扩展名一致

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 # 如果没有指定配置文件路径，bootstrap.yml
-CONFIG_LOCATION=${CONFIG_LOCATION:-"/app/config.yml"}
+CONFIG_LOCATION=${CONFIG_LOCATION:-"/app/config.yaml"}
 
 exec /app/algo-agent -conf ${CONFIG_LOCATION}
